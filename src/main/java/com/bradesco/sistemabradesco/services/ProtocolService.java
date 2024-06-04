b2744a1a6af81dc196b836e727e51068e581bac4
Mylena Santos <mylenarayane11@gmail.com>
2024-06-04 00:20:15 -0300

    Deploy da API usando o EC2 da AWS

--- src/main/java/com/bradesco/sistemabradesco/services/ProtocolService.java
+++ src/main/java/com/bradesco/sistemabradesco/services/ProtocolService.java
@@ -45,15 +45,16 @@ public class ProtocolService {
 
         Protocol protocol = new Protocol();
         ProtocolType protocolType = protocolDTO.getProtocolType();
-        Client client = clientService.addClient(protocolDTO.getClient());
+        if(protocolDTO.getClient() != null){
+            Client client = clientService.addClient(protocolDTO.getClient());
+            protocol.setClient(client);
+        }
         Channels channels = protocolDTO.getChannel();
-
         protocol.setCode(protocolDTO.getCode());
         protocol.setOpeningDate(LocalDate.now());
         LocalDate deadline = protocol.getOpeningDate().plusDays(protocolType.getTreatmentDeadline());
         protocol.setDeadline(deadline);
         protocol.setDescription(protocolDTO.getDescription());
-        protocol.setClient(client);
         protocol.setChannel(channels);
         protocol.setProtocolType(protocolType);
         protocol.setProtocolNumber(protocolDTO.getProtocolNumber());
